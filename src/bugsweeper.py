6bbcab6fc733f14804bb31b3f03905281dead678
Robert Meissner <robert.meissner@codecentric.de>
2022-05-25 21:18:15 +0200

    Refactoring tests to enable mock

--- src/bugsweeper.py
+++ src/bugsweeper.py
@@ -140,59 +140,7 @@ class MainWindow(QMainWindow):
 
         self.board_size, self.number_of_bugs = LEVELS[1]
 
-        w = QWidget()
-        hb = QHBoxLayout()
-
-        self.mines = QLabel()
-        self.mines.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
-
-        self.clock = QLabel()
-        self.clock.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
-
-        f = self.mines.font()
-        f.setPointSize(24)
-        f.setWeight(75)
-        self.mines.setFont(f)
-        self.clock.setFont(f)
-
-        self._timer = QTimer()
-        self._timer.timeout.connect(self.update_timer)
-        self._timer.start(1000)  # 1 second timer
-
-        self.mines.setText("%03d" % self.number_of_bugs)
-        self.clock.setText("000")
-
-        self.button = QPushButton()
-        self.button.setFixedSize(QSize(32, 32))
-        self.button.setIconSize(QSize(32, 32))
-        self.button.setIcon(QIcon(f"{image_folder}/smiley.png"))
-        self.button.setFlat(True)
-
-        self.button.pressed.connect(self.button_pressed)
-
-        l = QLabel()
-        l.setPixmap(QPixmap.fromImage(IMG_BOMB))
-        l.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
-        hb.addWidget(l)
-
-        hb.addWidget(self.mines)
-        hb.addWidget(self.button)
-        hb.addWidget(self.clock)
-
-        l = QLabel()
-        l.setPixmap(QPixmap.fromImage(IMG_CLOCK))
-        l.setAlignment(Qt.AlignLeft | Qt.AlignVCenter)
-        hb.addWidget(l)
-
-        vb = QVBoxLayout()
-        vb.addLayout(hb)
-
-        self.grid = QGridLayout()
-        self.grid.setSpacing(5)
-
-        vb.addLayout(self.grid)
-        w.setLayout(vb)
-        self.setCentralWidget(w)
+        self.initialize_board()
 
         self.init_map()
         self.update_status(STATUS_READY)
@@ -201,6 +149,51 @@ class MainWindow(QMainWindow):
         self.update_status(STATUS_READY)
 
         self.show()
+
+    def initialize_board(self):
+        hb = QHBoxLayout()
+        self.mines = QLabel()
+        self.mines.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+        self.clock = QLabel()
+        self.clock.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
+        f = self.mines.font()
+        f.setPointSize(24)
+        f.setWeight(75)
+        self.mines.setFont(f)
+        self.clock.setFont(f)
+        self._timer = QTimer()
+        self._timer.timeout.connect(self.update_timer)
+        self._timer.start(1000)  # 1 second timer
+        self.mines.setText("%03d" % self.number_of_bugs)
+        self.clock.setText("000")
+        self.button = QPushButton()
+        self.button.setFixedSize(QSize(32, 32))
+        self.button.setIconSize(QSize(32, 32))
+        self.button.setIcon(QIcon(f"{image_folder}/smiley.png"))
+        self.button.setFlat(True)
+        self.button.pressed.connect(self.button_pressed)
+        l = QLabel()
+        l.setPixmap(QPixmap.fromImage(IMG_BOMB))
+        l.setAlignment(Qt.AlignRight | Qt.AlignVCenter)
+        hb.addWidget(l)
+        hb.addWidget(self.mines)
+        hb.addWidget(self.button)
+        hb.addWidget(self.clock)
+        l = QLabel()
+        l.setPixmap(QPixmap.fromImage(IMG_CLOCK))
+        l.setAlignment(Qt.AlignLeft | Qt.AlignVCenter)
+        hb.addWidget(l)
+        vb = QVBoxLayout()
+        vb.addLayout(hb)
+        w = QWidget()
+        self.create_grid(vb, w)
+        self.setCentralWidget(w)
+
+    def create_grid(self, vb, w):
+        self.grid = QGridLayout()
+        self.grid.setSpacing(5)
+        vb.addLayout(self.grid)
+        w.setLayout(vb)
 
     def init_map(self):
         # Add positions to the map
@@ -262,15 +255,13 @@ class MainWindow(QMainWindow):
                         widget.click()
                 break
 
+    def get_surrounding_range(self, x: int):
+        return range(max(0, x - 1), min(x + 2, self.board_size))
+
     @measure
     def get_surrounding(self, x, y):
-        positions = []
-
-        for xi in range(max(0, x - 1), min(x + 2, self.board_size)):
-            for yi in range(max(0, y - 1), min(y + 2, self.board_size)):
-                positions.append(self.grid.itemAtPosition(yi, xi).widget())
-
-        return positions
+        return [self.grid.itemAtPosition(yi, xi).widget() for xi in self.get_surrounding_range(x) for yi in
+                self.get_surrounding_range(y)]
 
     @measure
     def get_surrounding_old(self, x, y):
--- tests/test_bugsweeper_test.py
+++ tests/test_bugsweeper_test.py
@@ -1,20 +1,27 @@
-from functools import wraps
-from time import time
+from unittest import mock
 from unittest.mock import MagicMock
+
+from PyQt5.QtWidgets import QApplication, QVBoxLayout, QHBoxLayout, QWidget
 
 from src.bugsweeper import MainWindow
 
 
 def test_get_surrounding():
-    window = MainWindow.__init__ = MagicMock()
-    board_mock = window()
-    board_mock.board_size = 5
+    with mock.patch("src.bugsweeper.MainWindow.update_status") as mocked_update_status:
+        with mock.patch("src.bugsweeper.MainWindow.reset_map") as mocked_reset_map:
+            with mock.patch("src.bugsweeper.MainWindow.show") as mocked_show:
+                app = QApplication([])
+                with mock.patch("src.bugsweeper.MainWindow.init_map") as mocked_init_map:
+                    window = MainWindow()
 
-    board_mock.get_surrounding = MainWindow.get_surrounding
-    assert len(board_mock.get_surrounding(board_mock, 0, 0)) == 4  # it is a corner
+                # Rather tedious right now
+                hb = QHBoxLayout()
+                vb = QVBoxLayout()
+                vb.addLayout(hb)
+                w = QWidget()
+                window.create_grid(vb, w)
+                window.init_map()
 
-    board_mock.get_surrounding = MainWindow.get_surrounding_old
-    assert len(board_mock.get_surrounding(board_mock, 0, 0)) == 4  # it is a corner
-
-    board_mock.get_surrounding = MainWindow.get_surrounding
-    assert len(board_mock.get_surrounding(board_mock, 0, 0)) == 4  # it is a corner
+    assert len(window.get_surrounding(0, 0)) == 4  # it is a corner
+    assert len(window.get_surrounding_old(0, 0)) == 4  # it is a corner
+    assert len(window.get_surrounding(0, 0)) == 4  # it is a corner
