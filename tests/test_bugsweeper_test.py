c0412659a487739e80f71ec8a8081eee619d4d32
Robert Meissner <robert.meissner@codecentric.de>
2022-05-25 17:53:27 +0200

    Adding tests for better comparability

--- tests/test_bugsweeper_test.py
+++ tests/test_bugsweeper_test.py
@@ -14,5 +14,7 @@ def test_get_surrounding():
     assert len(board_mock.get_surrounding(board_mock, 0, 0)) == 4  # it is a corner
 
     board_mock.get_surrounding = MainWindow.get_surrounding_old
+    assert len(board_mock.get_surrounding(board_mock, 0, 0)) == 4  # it is a corner
 
+    board_mock.get_surrounding = MainWindow.get_surrounding
     assert len(board_mock.get_surrounding(board_mock, 0, 0)) == 4  # it is a corner
